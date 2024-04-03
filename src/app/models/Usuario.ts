89e6785adef1802bff6e54b958fd457e65750dd5
plazadiazmario <plazadiazmario@gmail.com>
2024-04-03 13:51:49 +0200

    sin cromos

--- src/app/models/Usuario.ts
+++ src/app/models/Usuario.ts
@@ -3,15 +3,13 @@ export class Usuario {
     private _idUsuario: string;
     private _email: string;
     private _name: string;
-    private _cromos: Array<Cromo>;
     private _valoracion: number;
     private _imagenPerfil: string;
 
-    constructor(idUsuario: string, email: string, name: string, cromos: Array<Cromos>, valoracion: number, imagenPerfil: string) {
+    constructor(idUsuario: string, email: string, name: string, valoracion: number, imagenPerfil: string) {
         this._idUsuario = idUsuario;
         this._email = email;
         this._name = name;
-        this._cromos = cromos;
         this._valoracion = valoracion;
         this._imagenPerfil = imagenPerfil;
     }
@@ -38,14 +36,6 @@ export class Usuario {
 
     set name(value: string) {
         this._name = value;
-    }
-
-    get cromos(): Array<Cromos> {
-        return this._cromos;
-    }
-
-    set cromos(value: Array<Cromos>) {
-        this._cromos = value;
     }
 
     get valoracion(): number {
