d9dc5dee69ed437c5e36300a697e01340105884c
plazadiazmario <plazadiazmario@gmail.com>
2024-04-03 13:04:48 +0200

    creacion de carpetas

---
+++ src/app/components/login-google/login-google.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-google',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-google.component.html',
+  styleUrl: './login-google.component.css'
+})
+export class LoginGoogleComponent {
+
+}
