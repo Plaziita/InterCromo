8aa3e73de0692be6443c96d4c3140553532a56db
plazadiazmario <plazadiazmario@gmail.com>
2024-04-03 14:48:44 +0200

    favoritos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FavoritosComponent } from './components/favoritos/favoritos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FavoritosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
