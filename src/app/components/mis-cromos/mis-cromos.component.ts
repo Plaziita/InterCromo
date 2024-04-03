48a4c0e0eeeb93b461adaa1f55ebdfb33ebf3fe7
plazadiazmario <plazadiazmario@gmail.com>
2024-04-03 13:48:38 +0200

    components and user class

---
+++ src/app/components/mis-cromos/mis-cromos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-cromos',
+  standalone: true,
+  imports: [],
+  templateUrl: './mis-cromos.component.html',
+  styleUrl: './mis-cromos.component.css'
+})
+export class MisCromosComponent {
+
+}
