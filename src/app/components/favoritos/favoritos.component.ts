feb798cfe648f836ab818290fd4ec742c19f383b
Plaziita <plazadiazmario@gmail.com>
2024-04-04 14:59:54 +0200

    ee

--- src/app/components/favoritos/favoritos.component.ts
+++ src/app/components/favoritos/favoritos.component.ts
@@ -1,83 +1,24 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CromoService } from '../../../../../server/src/app/data-access/cromos/cromo.service';
 import { Cromo } from '../../models/Cromo';
 
 @Component({
   selector: 'app-favoritos',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './favoritos.component.html',
-  styleUrl: './favoritos.component.css',
+  styleUrls: ['./favoritos.component.css'],
 })
-export class FavoritosComponent {
-  cromos = [
-    {
-      nombre: 'Gyarados',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, adipisci asperiores, odit nobis voluptate sint quis illum consequatur eius voluptas, libero labore quae aliquid quasi? Mollitia dolorum facere libero iure!',
-      favorito: false,
-    },
-    {
-      nombre: 'Pikachu',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon número 25 en la Pokédex Nacional.',
-      favorito: false,
-    },
-    {
-      nombre: 'Charizard',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Charizard es un Pokémon de tipo fuego/volador introducido en la primera generación. Es la evolución de Charmeleon.',
-      favorito: true,
-    },
-    {
-      nombre: 'Bulbasaur',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Bulbasaur es un Pokémon de tipo planta/veneno introducido en la primera generación. Es el Pokémon número 1 en la Pokédex Nacional.',
-      favorito: false,
-    },
-    {
-      nombre: 'Mewtwo',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Mewtwo es un Pokémon legendario de tipo psíquico introducido en la primera generación. Es el Pokémon número 150 en la Pokédex Nacional.',
-      favorito: false,
-    },
-    {
-      nombre: 'Squirtle',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Squirtle es un Pokémon de tipo agua introducido en la primera generación. Es el Pokémon número 7 en la Pokédex Nacional.',
-      favorito: true,
-    },
-    {
-      nombre: 'Juanito',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Squirtle es un Pokémon de tipo agua introducido en la primera generación. Es el Pokémon número 7 en la Pokédex Nacional.',
-      favorito: false,
-    },
-    {
-      nombre: 'Pepon',
-      imagen:
-        'https://firebasestorage.googleapis.com/v0/b/intercromo-6a7d7.appspot.com/o/images%2FGyarados_Cromo.jpg?alt=media&token=d4986c90-978e-45e5-a756-54dfb05e8dac',
-      descripcion:
-        'Squirtle es un Pokémon de tipo agua introducido en la primera generación. Es el Pokémon número 7 en la Pokédex Nacional.',
-      favorito: true,
-    },
-  ];
+export class FavoritosComponent implements OnInit {
+  //cromos: any[];
 
-  toggleFavorito(cromo: Cromo) {
+  constructor(private cromoService: CromoService) {}
+
+  ngOnInit() {
+    this.cromoService.getCromos().subscribe((cromos) => {
+      //  this.cromos = cromos;
+    });
+  }
+
+  toggleFavorito(cromo: any) {
     cromo.favorito = !cromo.favorito;
     console.log(cromo.favorito);
   }
