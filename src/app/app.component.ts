3b2f113e1f6f3bdcc2a92cdeeb3f275aaa788b75
ChxuZou <chenxuan.zou2015@gmail.com>
2024-04-08 08:40:48 +0200

    Cambios en favoritos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { LogoComponent } from './components/logo/logo.component';
 import { BtnLogInComponent } from './components/btn-log-in/btn-log-in.component';
 import { FavoritosComponent } from './components/favoritos/favoritos.component';
 import { BarraBusquedaComponent } from './components/barra-busqueda/barra-busqueda.component';
+import { BarraNavegacionComponent } from './components/barra-navegacion/barra-navegacion.component';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ import { BarraBusquedaComponent } from './components/barra-busqueda/barra-busque
     RouterOutlet,
     FavoritosComponent,
     BarraBusquedaComponent,
+    BarraNavegacionComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
