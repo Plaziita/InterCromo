6cae58d52f3508db3717c36dd068bc5ab41cb9ad
plazadiazmario <plazadiazmario@gmail.com>
2024-04-09 08:53:40 +0200

    chatt

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [AngularFireAuth,AngularFireDatabase],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+
+  authState: Observable<any>;
+  newMessage: string = '';
+  messages: Observable<any[]>;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase
+  ) {
+    this.authState = this.afAuth.authState;
+  }
+
+  ngOnInit(): void {
+    this.messages = this.db.list('messages').valueChanges();
+  }
+
+  async sendMessage() {
+    const user = await this.afAuth.currentUser;
+    if (user) {
+      const message = {
+        username: user.displayName,
+        text: this.newMessage
+      };
+      this.db.list('messages').push(message);
+      this.newMessage = '';
+    }
+  }
 
 }
+
+
